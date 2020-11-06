ddc23fafc912e41686ee8256bf2bdf89bb5e387b
hanxuewei <han.xuewei@zte.com.cn>
2020-11-06 10:47:06 +0800

    Fix the bug of model_compiler_2
    Closes #344, #345.
    
    Signed-off-by: hanxuewei <han.xuewei@zte.com.cn>

--- model_compiler_2/src/model_compiler/models/sources/tf_model_file.py
+++ model_compiler_2/src/model_compiler/models/sources/tf_model_file.py
@@ -1,7 +1,6 @@
 # Copyright 2019 ZTE corporation. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from os import path
 from typing import Any, Mapping, NamedTuple
 
 from .. import repository
@@ -23,16 +22,16 @@ class TfModelFile(NamedTuple):
     def accepts_kwargs(kwargs: Mapping[str, Any]) -> bool:
         input_model = kwargs.get('model_path')
 
-        return isinstance(input_model, str) and input_model[-5:].upper() == '.CKPT' and path.isfile(input_model)
+        return isinstance(input_model, str) and input_model[-5:].upper() == '.CKPT'
 
     @staticmethod
     def accepts_json(value: Mapping[str, Any]) -> bool:
         input_model = value.get('input_model')
 
-        return isinstance(input_model, str) and input_model.lower().endswith('.ckpt')
+        return isinstance(input_model, str) and input_model[-5:].upper() == '.CKPT'
 
     @staticmethod
     def accepts_env(env: Mapping[str, str]) -> bool:
         input_model = env.get('CHECKPOINT_PATH')
 
-        return isinstance(input_model, str) and path.isfile(input_model)
+        return isinstance(input_model, str) and input_model[-5:].upper() == '.CKPT'
